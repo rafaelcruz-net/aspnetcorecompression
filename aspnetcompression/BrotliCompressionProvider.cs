84a7ed0865978018a7bd4757f2a03158e63844b4
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2019-12-20 14:42:52 -0300

    Iniital Commit

---
+++ aspnetcompression/BrotliCompressionProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.ResponseCompression;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnetcompression
+{
+    public class BrotliCompressionProvider : ICompressionProvider
+    {
+        public string EncodingName => "br";
+
+        public bool SupportsFlush => true;
+
+        public Stream CreateStream(Stream outputStream) => new BrotliStream(outputStream, CompressionLevel.Fastest);
+
+    }
+}
